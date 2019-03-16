a40b0f9f9411c357c7d995573dd479dc5a0ae4a0
anon <anon@anon.com>
2019-03-16 00:07:41 -0400

    first commit

---
+++ bot.py
@@ -0,0 +1,101 @@
+import praw
+from res import look, min_break, summon
+import random
+import time
+# TODO Maybe add support for thesaurus lookups to produce more undefined behavior
+
+# 03/15/19 get login working
+
+# client_id eDZ6LmgUNC24Jw
+# client_secret P5MTRkvIFkuIFvU-lRdwr-epgIM
+# username HedonismBotBot
+# password djambichocolateicing
+
+
+def getRandomQuote():
+    l = random.choice(look.values())  # Get random set of quotes
+    return random.choice(l)  # return random quote from that list
+
+
+def getQuoteFromContext(context):
+    # Get all the keys from the look up and try to match words to them
+    for k in look.keys():
+        for e in k:
+            if e in context.lower():
+                return random.choice(look[k])
+    # Otherwise get a random quote
+    return None
+
+
+def timeLimitPost(msg, p):
+    # Only print if we have exceeded the min break
+    if time.time() - last_post > min_break:
+        p.reply(msg)
+        last_post = time.time()
+
+
+if __name__ == '__main__':
+    reddit = praw.Reddit(client_id='eDZ6LmgUNC24Jw', client_secret='P5MTRkvIFkuIFvU-lRdwr-epgIM',
+                         username='HedonismBotBot', password='djambichocolateicing', user_agent='test for /u/HedonismBotBot')
+
+    subreddit = reddit.subreddit('HedonisticPosts')
+
+    print(subreddit.display_name)
+    print(subreddit.title)
+    print(subreddit.description)
+
+    comments = subreddit.stream.comments()
+
+    last_post = 0 # Will try to automatically post when it starts
+
+    posts_visited = list() # List of the posts we have already posted in automatically.
+
+    for comment in comments:
+
+        print("Post Title: " + comment.submission.title + '\n\n')
+
+        un = comment.submission.author.name
+
+        print("Tick")
+
+        if getQuoteFromContext(comment.submission.title) != None:
+            # Get quote based on title of post
+            msg = getQuoteFromContext(comment.submission.title).format(un)
+
+            # Post the quote as a top level comment
+            if time.time() - last_post > min_break:
+                print("Replying to this post because of the title with: {}\n".format(msg))
+                comment.submission.reply(msg)
+                last_post = time.time()
+                
+        if comment.body == summon:
+            try:
+                parent_comment = reddit.comment(
+                    id=comment.parent_id.split('_')[-1])
+                if getQuoteFromContext(parent_comment.body) != None:
+                    msg = getQuoteFromContext(
+                        parent_comment.body).format(parent_comment.author)
+                else:
+                    msg = getRandomQuote().format(parent_comment.author)
+                print("I was summoned to comment on upper comment with: {}".format(msg))
+                comment.reply(msg)
+            except:
+                continue
+        elif summon in comment.body:
+            if getQuoteFromContext(comment.body) != None:
+                msg = getQuoteFromContext(comment.body).format(comment.author)
+            else:
+                msg = getRandomQuote().format(comment.author)
+            print("Replying to a user's request to hedbot themselves with: {}".format(msg))
+            comment.reply(msg)
+        # Automatic posting option, happens once per hour and only once per post
+        elif getQuoteFromContext(comment.body) != None:
+            if time.time() - last_post > min_break and comment.submission.id not in posts_visited:
+                msg = getQuoteFromContext(comment.body)
+                print("Replying automatically to a post with: {}".format(msg))
+                comment.replay(msg)
+                last_post = time.time()
+                posts_visited.append(comment.submission.id)
+            
+
+
---
+++ res.py
@@ -0,0 +1,57 @@
+# Resource variables for the bot
+
+## Catagories of Hedonism ##
+# Luxury
+luxury_keywords = ('guchi', 'expensive', 'decadent',
+                   'gourgious', 'gold', 'gems', 'spice', )
+luxury_quotes = ('It seems {} hates humans the way I hate having my nipples polished with industrial sand paper.',
+                 'Nothing but the most expensive and decadent scented towels would satisfy {} tastes. How wonderful!',
+                 'I have no use for it anymore {}, it has done everyone and everything.')
+
+
+# Sex
+sex_keywords = ('sex', 'orgy', 'laid', 'lust', 'hot',
+                'sordid', 'unconventional love', 'threesome', 'hedonist', 'hedonistic')
+sex_quotes = ('Hello, {}. Might I procure your services? Oh, nothing sordid, I assure you! Simply vomit on me, ever so gently, while I humiliate a pheasant.',
+              'I too have known unconventional love. Perhaps {} and I... and Djambi, can get together and compare notes sometime.',
+              'Ooooooo! Room for one more {}? ',
+              'I trust the orgy pit has been scraped and buttered {}!')
+
+# Gluttony
+gluttony_keywords = ('stuffed', 'good food', 'feast',
+                     'gorge', 'starving', 'chocolate', 'grapes')
+gluttony_quotes = ('And just when I was beginning to lose interest {}... Djambi, the chocolate icing!',
+                   'One can never have too many candied nuts! Isn\'t that right {}?',
+                   'Let the wine flow like the tears of one-thousand boiled geese {}!')
+
+
+# Relaxation
+relaxation_keywords = ('nap', 'lounge', 'hang out',
+                       'lay down', 'relax', 'relaxation', 'relaxing')
+relaxation_quotes = ('Let us cavort like the Greeks of old! You know the ones I mean {} :)',
+                     'There\'s no debauchery like end-of-the-world-debauchery! {}\'s lips, my lips, apocalypse, oo-whoo!')
+
+# Entertainment
+entertainment_keywords = ('party', 'rager', 'play',
+                          'movie', 'attention', 'interest', 'awesome', 'cool', 'rad')
+entertainment_quotes = ('Let the games begin {}!',
+                        'And just when I was beginning to lose interest {}... Djambi, the chocolate icing!')
+
+
+# Vanity
+vanity_keywords = ('my picture', 'my face', 'gourgious selfie',
+                   'beautiful selfie', 'hot selfie', 'look at me')
+vanity_quotes = ('{}\'s latest performance was as delectable as dipping my bottom over and over into a bath of the silkiest oils and creams.',
+                 'I appologize for nothing {}!',
+                 '{}\'s face is more scrumptious than Djambi\'s finest brasserie!',
+                 'And just when I was beginning to lose interest {}... Djambi, the chocolate icing!')
+
+# Lookup dictionary
+look = {luxury_keywords: luxury_quotes, sex_keywords: sex_quotes, gluttony_keywords: gluttony_quotes,
+        relaxation_keywords: relaxation_quotes, entertainment_keywords: entertainment_quotes, vanity_keywords: vanity_quotes}
+
+# Summoning charm
+summon = '!hedbot'
+
+# Post timer. Limit automatic responses to once in this period.
+min_break = 3600 #s in one hour
